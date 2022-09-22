134af1fa7452c4620cafc0a629aac1ef89a89642
gitpk-0 <kell108p@protonmail.com>
2022-09-22 19:41:27 -0400

    type & rating forms

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
+import useStyles from "./styles";
+
 const Header = () => {
+  const classes = useStyles(); // calling the useStyles hook
   return (
     // nav bar layout
     <AppBar position="static">
@@ -13,19 +16,19 @@ const Header = () => {
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
-            Travel the world
+            Start your next adventure
           </Typography>
-          <Autocomplete>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search... "
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Autocomplete>
+            <InputBase
+              placeholder="Search... "
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
 
 const List = () => {
-    return (
-        <h1>List</h1>
-    );
-}
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
+  const places = [
+    { name: "Nice Place" },
+    { name: "Best Drinks" },
+    { name: "Best Meat" },
+  ];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">
+        Restaurants, Hotels, & Attactions near you
+        <FormControl className={classes.formControl}>
+          {/* Type of activity drop down menu */}
+          <InputLabel>Type</InputLabel>
+          <Select value={type} onChange={(e) => setType(e.target.value)}>
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          {/* Filter by rating drop down menu */}
+          <InputLabel>Rating</InputLabel>
+          <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>3+ Stars</MenuItem>
+            <MenuItem value={3.5}>3.5+ Stars</MenuItem>
+            <MenuItem value={4}>4+ Stars</MenuItem>
+            <MenuItem value={4.5}>4.5+ Stars</MenuItem>
+          </Select>
+        </FormControl>
+        <Grid container spacing={3} className={classes.list}>
+          {/* if there are places, map over them */}
+          {places?.map((place, i) => {})}
+        </Grid>
+      </Typography>
+    </div>
+  );
+};
 
 export default List;
