f2da779803c7302cf67bdf218a2f1257bdc47ad0
gitpk-0 <kell108p@protonmail.com>
2022-09-22 18:27:52 -0400

    readme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
