ac82c6220caec7ac072147535bab0ac66f1ffdc8
gitpk-0 <76492959+gitpk-0@users.noreply.github.com>
2022-09-27 19:45:29 -0400

    update 9.27

--- src/api/adventureAmigoAPI.js
+++ src/api/adventureAmigoAPI.js
@@ -17,8 +17,7 @@ export const getPlacesData = async (type, sw, ne) => {
           tr_longitude: ne.lng, // top right longitude
         },
         headers: {
-          "X-RapidAPI-Key":
-            "e404f09c61msh3995674732fea40p151e88jsn1130448610d2",
+          "X-RapidAPI-Key": process.env.REACT_APP_RAPIDAPI_TRAVEL_API_KEY,
           "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
         },
       }
@@ -36,15 +35,19 @@ export const getWeatherData = async (lat, lng) => {
   try {
     let latt = lat.toFixed(2);
     let long = lng.toFixed(2);
-    const apiKey = "baa163b0c95b6696f3c24313452bd64e";
+    const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
     const { data } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latt}&lon=${long}&appid=${apiKey}`
     );
-    console.log(data.weather[0].icon);
-    console.log(`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    // console.log(data.coord.lat);
+    // console.log(data.weather[0].icon);
+    console.log(
+      `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
 
     return data;
   } catch (error) {
+    console.log("getWeatherData error");
     console.log(error);
   }
 };
